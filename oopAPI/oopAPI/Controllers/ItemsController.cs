d2d77516693589df5009b9f1ed3531caaa60e067
Abdelrahman Ibrahim <129771097+bodzx1@users.noreply.github.com>
2024-04-25 19:22:24 +0200

    Add files via upload

---
+++ oopAPI/oopAPI/Controllers/ItemsController.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using oopAPI.Interfaces;
+using oopAPI.Models;
+namespace oopAPI.Controllers
+
+{
+    [Route("api/Items")]
+    [ApiController]
+    public class ItemsController : Controller
+
+    {
+        private readonly IItemsRepositary _itemsRepositary;
+        public ItemsController(IItemsRepositary itemsRepositary)
+        {
+            _itemsRepositary = itemsRepositary;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Items>))]
+        public IActionResult Getitems()
+        {
+            var items = _itemsRepositary.GetItems();
+            if (!ModelState.IsValid)
+                return BadRequest();
+            return Ok(items);
+        }
+
+
+
+    }
+}
