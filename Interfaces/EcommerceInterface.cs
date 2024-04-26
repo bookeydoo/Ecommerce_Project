a30463690a77cf9ec0411fd469c90dea6cdb156b
Waleed Amgad <141445359+bookeydoo@users.noreply.github.com>
2024-04-26 19:30:01 +0300

    small changes

--- Interfaces/EcommerceInterface.cs
+++ Interfaces/EcommerceInterface.cs
@@ -1,11 +1,25 @@
 using ecommerce_api.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ecommerce_api.Interfaces
 {
     public interface EcommerceInterface
     {
         ICollection<logininfo> GetLogininfos();
-        ICollection<Storeitems> GetStores();
+        ICollection<Storeitems> GetStoreItems();
 
+        logininfo GetLogininfo(string FirstName);
+        
+        Storeitems GetStoreitems_id(int id);
+        Storeitems GetStoreitems_name( string name );
+
+        //bool CreateItem(string name,int id,int price,Storeitems storeitem);
+        //bool UpdateItem(string name, int id, int price, Storeitems storeitem);
+
+        //bool CreateUser(string FirstName, string LastName, string password);
+        
+
+        public bool Save();
+        
     }
 }
