2bf8be1a59ac8cd11b6f9531fb29126d1bf75c82
Abdelrahman Ibrahim <129771097+bodzx1@users.noreply.github.com>
2024-04-26 16:26:30 +0300

    NEW FOLDERS

---
+++ Interfaces/UserData.cs
@@ -0,0 +1,10 @@
+﻿using ecommerce_api.Models;
+
+namespace ecommerce_api.Interfaces
+{
+        public interface IUserDataRepo
+        {
+            public ICollection<UserData> GetItems();
+        }
+    
+}
